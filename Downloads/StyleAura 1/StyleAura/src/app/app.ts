b353abb499c0a2712b9da6a1f3f22efed349ac4f
sakthi23102003 <139611638+sakthi23102003@users.noreply.github.com>
2025-09-25 15:27:56 +0800

    Add files via upload

--- Downloads/StyleAura 1/StyleAura/src/app/app.ts
+++ Downloads/StyleAura 1/StyleAura/src/app/app.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Header from './shared/header/header';
 import Footer from './shared/footer/footer';
+import { OurproductsComponent } from './ourproducts/ourproducts';
+import { ProductDetails } from './product-details';
+import { CommonModule } from '@angular/common';
+import Home from './home/home';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer,OurproductsComponent, CommonModule, Home, HttpClientModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
